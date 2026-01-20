ecb08bf07b77398df2f5e9d1cf4ae663c7271b87
Mayur Sangave <sangavemayur5@gmail.com>
2026-01-20 23:35:19 +0530

    Series Generation

--- SeriesGenerator.java
+++ SeriesGenerator.java
@@ -21,31 +21,27 @@ Sample Output
 
 import java.util.Scanner;
 
-class Solution{
-    public static void main(String []argh){
+public class SeriesGenerator {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt(); // number of queries
+
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
-            //System.out.println(a);
-            //System.out.println(b);
-            //System.out.println(n);
-            //a+b*2^i
-            int p=1;
-            for(int j=0;j<n;j++){
-                a+=b*p;
-                System.out.print(a+" ");
-                p<<=1;
-            }  
-            
-            System.out.println();
-            
+
+            int p = 1; // represents 2^0
+
+            for (int j = 0; j < n; j++) {
+                a += b * p;
+                System.out.print(a + " ");
+                p <<= 1; // multiply p by 2 for next power
+            }
+
+            System.out.println(); // move to next line for next query
         }
-        
-        
-        
+
         in.close();
     }
 }
