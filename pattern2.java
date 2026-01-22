ef215722880b74b52d8ff3a37d4cad02ba372d7a
mayursangave5 <sangavemayur5@gmail.com>
2026-01-22 23:58:26 +0530

    pattern type 2

---
+++ pattern2.java
@@ -0,0 +1,26 @@
+/*
+* * * * *
+*       *
+*       *
+* * * * *      
+
+*/
+
+public class pattern2 {
+    public static void main(String[] args) {
+        int r =4;
+        int c =5;
+       for (int i=1 ;i<=r;i++){
+            System.out.println();
+             for (int j=1 ;j<=c;j++){
+                if(i==1 ||j==1 || i==r || j==c) {
+                System.out.print("* ");
+                } else{
+                 System.out.print("  ");
+                                          }
+            }
+        }
+
+    }
+    
+}
